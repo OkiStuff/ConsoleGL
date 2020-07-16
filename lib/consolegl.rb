1dd04000eea94285b61a71e3f271541a3dbc1afb
okistuff <probro1802@gmail.com>
2020-07-16 15:46:55 -0400

    💻 Updated to 1.0.7

--- lib/consolegl.rb
+++ lib/consolegl.rb
@@ -1,41 +1,58 @@
-"""
-ConsoleGL Version 1.0 || DOCS
+#ConsoleGL Version 1.0 || DOCS
 
-class: Color
+#class: Color
     # Color IDs
-    RED
-    GREEN
-    YELLOW
-    BLUW
-    LIGHT_BLUE
-    WHITE
+    #RED
+    #GREEN
+    #YELLOW
+    #BLUE
+    #LIGHT_BLUE
+    #WHITE
 
-class: Console
-    addColor(x, s):
-        Adds Color to string or number
-        Params:
-            X = COLOR ID
-            S = STRING OR NUMBER
-        Usage:
-            addColor(Color.RED, 'ERROR')
-    LineVertical(color):
-        Returns a Vertical Line
-        Params:
-            Color = COLOR ID
-        Usage:
-            LineVertical(Color.BLUE)
+#class: Console
+    #addColor(x, s):
+        #Adds Color to string or number
+        #Params:
+            #X = COLOR ID
+            #S = STRING OR NUMBER
+        #Usage:
+            #addColor(Color.RED, 'ERROR')
+    #LineVertical(color):
+        #Returns a Vertical Line
+        #Params:
+            #Color = COLOR ID
+        #Usage:
+            #LineVertical(Color.BLUE)
 
-"""
+
 class Color
 
     ## COLOR VALUE ##
 
-    RED = 31
-    GREEN = 32
-    YELLOW = 33
-    BLUE = 34
-    LIGHT_BLUE = 36
-    WHITE = false
+    def self.RED
+        return 31
+    end
+
+    def self.GREEN
+        return 32
+    end
+
+    def self.YELLOW
+        return 33
+    end
+
+    def self.BLUE
+        return 34
+    end
+
+    def self.LIGHT_BLUE
+        return 36
+    end
+
+    def self.WHITE
+        return false
+    end
+
 
 end
 
@@ -45,8 +62,9 @@ class Console
 
     def self.addColor(x,s)
 
-        if x != false:
+        if x != false
             "\e[#{x}m#{s.to_s}\e[0m"
+        end
     end
     
     
@@ -67,6 +85,7 @@ class Console
 
     def self.LineHorizontal(color)
         return addColor(color, "│")
+    end
 
     def self.BigSquareMiddle(color)
         return addColor(color, "■")
@@ -130,11 +149,10 @@ class Console
 
 
     def SetConsoleColor(value)
-        system "COLOR " + value.to_s +
+        system "COLOR " + value.to_s
     end
 
     def ResetConsoleColor()
         system "COLOR 07"
     end
-
 end
